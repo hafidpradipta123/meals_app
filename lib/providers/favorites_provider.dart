01deca483bb712ed0372dce324c27b7f05495587
Hafid Pradipta <pradipta.hafid@gmail.com>
2023-07-25 05:20:30 +0800

    first commit

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/models/meal.dart';
+
+class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavouriteMealsNotifier() : super([]);
+
+  bool toggleMealFavouriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state
+          .where((m) => m.id != meal.id)
+          .toList(); // filter a list and get a new list.
+          return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
+  }
+}
+
+final favouriteMealsProvider = StateNotifierProvider<FavouriteMealsNotifier, List<Meal>>((ref) {
+  return FavouriteMealsNotifier ();
+});
---
+++ lib/screens/meal_details.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/providers/favorites_provider.dart';
+class MealDetailsScreen extends ConsumerWidget{
+
+  const MealDetailsScreen({super.key,
+  required this.meal,});
+
+
+  final Meal meal;
+
+  
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: Text(meal.title),
+      actions:[
+        IconButton(onPressed: (){
+          final wasAdded =
+          ref
+        .read(favouriteMealsProvider.notifier)
+        .toggleMealFavouriteStatus(meal);
+            ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(wasAdded ?'Meal added as a favourite!' : 'Meal removed')));
+        },
+         icon: const Icon(Icons.star),)
+      ],),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+      
+            const SizedBox(height: 14,),
+            Text("Ingredients", style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color:Theme.of(context).colorScheme.primary, fontWeight: FontWeight.bold
+            ),),
+      
+            const SizedBox(height: 14,),
+            for (final ingredient in meal.ingredients)
+            Text(ingredient,  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              color:Theme.of(context).colorScheme.onBackground)
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+                  Text(
+              "Steps",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+             for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+                child: Text(step, 
+                textAlign: TextAlign.center,
+                 style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color:Theme.of(context).colorScheme.onBackground)
+                        ),
+              ),
+      
+      
+        ],),
+      ),  
+    );
+  }
+
+}
